72bb909c9530f63393be4de0e85dfbb00d378ef7
guille <guillermoneculqueo@gmail.com>
2023-04-22 01:05:48 -0300

    solucionando cosas

--- conect.php
+++ conect.php
@@ -1,5 +1,8 @@
 <?php
 
-// $conexion = mysqli_connect("localhost", "root", "po92ke14", "phpintermedio")  or die("Error " . mysqli_error($conexion));
+$conexion = mysqli_connect("containers-us-west-188.railway.app", "root", "hZmqafXAN4cmT4i9HqLx", "railway")  or
+  die("Error " . mysqli_error($conexion));;
 
-$conexion = mysqli_connect("containers-us-west-188.railway.app", "root", "hZmqafXAN4cmT4i9HqLx", "railway")  or die("Error " . mysqli_error($conexion));
+// $conexion = mysqli_connect("localhost", "root", "po92ke14", "phpintermedio")  or
+// die("Error " . mysqli_error($conexion));
+// ;
